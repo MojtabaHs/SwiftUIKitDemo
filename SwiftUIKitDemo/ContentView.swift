dfe4451aadf07711938667ed362e90867122e7c9
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2020-09-24 14:45:49 +0330

    Initial Commit

---
+++ SwiftUIKitDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIKitDemo
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 9/24/20.
+//  
+//
+//  StackOverflow: https://stackoverflow.com/story/mojtabahosseini
+//  Linkedin: https://linkedin.com/in/MojtabaHosseini
+//  GitHub: https://github.com/MojtabaHs
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIKitDemo/SwiftUIKitDemoApp.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIKitDemoApp.swift
+//  SwiftUIKitDemo
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 9/24/20.
+//  
+//
+//  StackOverflow: https://stackoverflow.com/story/mojtabahosseini
+//  Linkedin: https://linkedin.com/in/MojtabaHosseini
+//  GitHub: https://github.com/MojtabaHs
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
